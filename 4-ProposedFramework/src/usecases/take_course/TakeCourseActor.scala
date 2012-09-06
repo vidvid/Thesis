ed6e420536ff4959bc41c6270c1198ca4800a202
Vahid Zoghi <vahid.zoghi@gmail.com>
2012-09-07 04:08:56 +0430

    finished use cases

--- 4-ProposedFramework/src/usecases/take_course/TakeCourseActor.scala
+++ 4-ProposedFramework/src/usecases/take_course/TakeCourseActor.scala
@@ -1,23 +1,39 @@
 class StudentTakeCourseActor(
-  var student: Student, course: Course, target: Actor) extends Actor {
+  val student: Student, course: Course, target: Actor) extends Actor {
+  var receivedResponses:Int = 0
     override def act() {
-      student !  HasPassedRequest(this, course)
-    loop {
-      react {
-        case HasPassedResponse(course, true) =>
-          //studen has already passed the course.
-        case HasPassedResponse(course, false) =>
-          //student has not already passed the course
-      }
-    }
+      student ! PassedRequest(course)
+      student ! TakenRequest(course)
+      student ! PassedPresRequest(course)
+      new UnitValidationActor(this, student, course).start
+    loop {  react {
+        case PassedPresResponse(result) =>
+          if(!result)
+            sendResponse(false, "Student has not passed prerequisites")
+          else waitForNextMessage()
+        case PassedResponse(result) =>
+          if(result)
+            sendResponse(false, "Student has already passed this course")
+          else waitForNextMessage()
+        case TakenResponse(result) =>
+          if(result)
+            sendResponse(false, "Student has already taken this course")
+          else waitForNextMessage()
+        case UnitsValidationResponse(result) =>
+          if(!result)
+            sendResponse(false, "Student can't take more than 20 units")
+          else waitForNextMessage()
+      } }
   }
-
   def sendResponse(result: Boolean, comment: String) {
     target ! TakeCourseResponse(result, comment)
-//    println("sent "  + TakeCourseResponse(result, comment) + " to " + target)
     exit
   }
-
-  override def toString = "[StudentTakeCourseActor of " + student + "]"
-
+  def waitForNextMessage() {
+    receivedResponses++
+    if(receivedResponses == 4) {
+      //save a StudyRecord for course
+      sendResponse(true, "successful")
+    }
+  }
 }
