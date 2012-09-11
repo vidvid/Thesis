72c59d431add6c0463149f1d74c35eba287d04f1
Vahid Zoghi <vahid.zoghi@gmail.com>
2012-09-11 04:54:19 +0430

    finished main body, reviewing is needed, actor diagrams need to be consistent

--- 4-ProposedFramework/src/usecases/take_course/TakeCourseActor.scala
+++ 4-ProposedFramework/src/usecases/take_course/TakeCourseActor.scala
@@ -5,7 +5,6 @@ class StudentTakeCourseActor(
       student ! PassedRequest(course)
       student ! TakenRequest(course)
       student ! PassedPresRequest(course)
-      new UnitValidationActor(this, student, course).start
     loop {  react {
         case PassedPresResponse(result) =>
           if(!result)
@@ -19,10 +18,6 @@ class StudentTakeCourseActor(
           if(result)
             sendResponse(false, "Student has already taken this course")
           else waitForNextMessage()
-        case UnitsValidationResponse(result) =>
-          if(!result)
-            sendResponse(false, "Student can't take more than 20 units")
-          else waitForNextMessage()
       } }
   }
   def sendResponse(result: Boolean, comment: String) {
@@ -31,7 +26,7 @@ class StudentTakeCourseActor(
   }
   def waitForNextMessage() {
     receivedResponses++
-    if(receivedResponses == 4) {
+    if(receivedResponses == 3) {
       //save a StudyRecord for course
       sendResponse(true, "successful")
     }
